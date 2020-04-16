ad657c02ead4b5a64340e57440b0a65b0846199b
caizhijian <caizhijian@sinobest.cn>
2020-04-16 16:57:39 +0800

    修改logout

--- src/main/java/com/czj/modules/cas/controller/CASController.java
+++ src/main/java/com/czj/modules/cas/controller/CASController.java
@@ -25,7 +25,7 @@ public class CASController {
         //销毁session
         session.invalidate();
         //使用cas退出成功后,跳转到http://cas.client1.com:9001/logout/success
-        return "redirect:http://cas.example.org:8080/cas/logout?service=http://myweb.example.org:8998/cas/logout/success";
+        return "redirect:http://cas.example.org:8080/cas/logout?service=http://myweb.example.org:8998/webclient/cas/logout/success";
     }
 
     /**
